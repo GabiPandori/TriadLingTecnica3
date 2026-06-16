98e8e91ef8753a756e2d3f4f40b4736e189c547c
Gabriela <gabriela.pandori@aluno.ifsp.edu.br>
2026-06-16 19:06:51 -0300

    alterações frase diaria

--- dao/FraseDiariaDao.py
+++ dao/FraseDiariaDao.py
@@ -6,14 +6,13 @@ class FraseDiariaDao:
             2: {"frase": "Cada dia é uma nova oportunidade.", "data": "2026-06-02"},
             3: {"frase": "Sorria para a vida!", "data": "2026-06-03"}
         }
- #FRASES ESPECIFICAS
+
     def obterFrase(self, data):
         for frase_info in self.fraseDiarias.values():
             if frase_info["data"] == data:
                 return frase_info
         return None
 
-#TODAS AS FRASES
     def listarFrases(self):
         listaFrases = list(self.fraseDiarias.values())
         return listaFrases
